a9266d487a02828d8f61c2180cf664069d1db85b
justamanwho <dmytro.kostuyk@gmail.com>
2024-09-14 20:27:24 +0200

    added couple photos/reply func/requirements/token

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# digits-bot
+The bot, that waits for the user to send the number out of 9 and responds as I see those numbers.
+
+1 - One Punch Man
+2 - Jotaro x Dio
+3 - Part 3 Jotaro
+4 - MISTAAAAAAA
+5 - 
+6 - Stone Ocean
+7 - Lucky
+8 - Dio plays Chess
+9 - 
+
+
+
+If you see any jojo references here, come to the therapist, there is no such thing.
--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-from telebot import types
+from telebot import TeleBot, types
+from PIL import Image
+
+token = open('token.txt').readline()
+bot_name = 'my_digits_bot'
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+bot = TeleBot(token)
+
+
+digits, str_digits = [i for i in range(10)], [str(i) for i in range(10)]
+
+
+@bot.message_handler(commands=str_digits)
+def message_reply(message):
+    text = message.text
+    if text in str_digits:
+        try:
+            img = Image.open(f'digits-photos/{text}.png')
+        except FileNotFoundError:
+            img = Image.open(f'digits-photos/{text}.jpg')
+        bot.send_photo(message.chat.id, img)
+
+    elif text in str_digits and len(text) == 2 and '/' == text[0]:
+        try:
+            img = Image.open(f'digits-photos/{text[1]}.png')
+        except FileNotFoundError:
+            img = Image.open(f'digits-photos/{text[1]}.jpg')
+
+        bot.send_photo(message.chat.id, img)
+
+    else:
+        img = Image.open(f'digits-photos/wtf-am-i-reading.jpg')
+
+        bot.reply_to(message, 'Choose the digit from 0 to 9', reply_markup=markup)
+        bot.send_photo(message.chat.id, img)
+
+
+
+
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
