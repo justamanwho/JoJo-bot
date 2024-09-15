9ab073e7a7262aaad89576be68ffbe04e79ec489
justamanwho <dmytro.kostuyk@gmail.com>
2024-09-15 21:21:38 +0200

    added every digit reference/constracted image openings logic/made easter egg responses

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from telebot import TeleBot, types
 from PIL import Image
+import re
+
 
 token = open('token.txt').readline()
 bot_name = 'my_digits_bot'
@@ -11,36 +13,103 @@ digits, str_digits = [i for i in range(10)], [str(i) for i in range(10)]
 
 
 def error_handling(message):
-    img = Image.open(f'digits-photos/wtf-am-i-reading.jpg')
-
     bot.reply_to(message, 'Choose the digit from 0 to 9', reply_markup=markup)
-    bot.send_photo(message.chat.id, img)
+
+    return send_photo_by_name_extention(message, 'wtf-am-i-reading', 'jpg')
+
+
+def wtf_reading(text, message):
+    pattern = re.compile(
+        r'(am\s*i\s*reading|what\s*the\s*fuck|wtf)\s*(am\s*i\s*reading)?[\s,?]*|'
+        r'(am\s*i\s*reading)?\s*(what\s*the\s*fuck|wtf)[\s,?]*|'
+        r'(what\s*are\s*you\s*talking\s*about)',
+        re.IGNORECASE
+    )
+
+    if pattern.match(text):
+        # change func name
+        return send_photo_by_name_extention(message, 'your_next_line', 'png')
+
+    return False
+
+
+# make gif load faster
+def the_world(text, message):
+    # make it re
+    message_possibilities = ['the world', 'za warudo', 'the world!', 'za warudo!']
+
+    if text.lower() in message_possibilities:
+        send_photo_by_name_extention(message, 'jojo-the-world', 'gif')
+        return True
+
+    return False
+
+
+def omg(text, message):
+    pattern = re.compile(
+        r'\s*(omg|oh[\W]*my[\W]*god|oh[\W]*shit[\W]*)\s*!?',
+        re.IGNORECASE
+    )
+
+    if pattern.match(text):
+        return send_photo_by_name_extention(message, 'omg', 'jpg')
+
+    return False
+
+
+
+def yare_yare(text, message):
+    pattern = re.compile(r'(yare+\s*?yare+\s?|daze)', re.IGNORECASE)
+
+    if pattern.match(text):
+        return send_photo_by_name_extention(message, 'yare-yare', 'jpg')
+
+    return False
+
+
+def send_photo_by_name(message, name):
+    extensions = ['jpg', 'png', 'gif']
+
+    for ext in extensions:
+        try:
+            return send_photo_by_name_extention(message, name, ext)
+        except FileNotFoundError:
+            continue
+
+    return error_handling(message)
+
+
+def send_photo_by_name_extention(message, name, ext):
+    img = Image.open(f'static/{name}.{ext}')
+
+    if ext == 'gif':
+        with open(f'static/{name}.{ext}', 'rb') as gif_file:
+            return bot.send_animation(message.chat.id, gif_file)
+    else:
+        return bot.send_photo(message.chat.id, img)
 
 
 @bot.message_handler()
 def message_reply(message):
     text = message.text
     if text in str_digits:
-        try:
-            img = Image.open(f'digits-photos/{text}.png')
-        except FileNotFoundError:
-            try:
-                img = Image.open(f'digits-photos/{text}.jpg')
-            except FileNotFoundError:
-                return error_handling(message)
+        send_photo_by_name(message, text)
 
-        bot.send_photo(message.chat.id, img)
+    elif len(text) == 2 and text[1] in str_digits:
+        send_photo_by_name(message, text[1])
 
-    elif len(text) == 2 and '/' == text[0] and text[1] in str_digits:
-        try:
-            img = Image.open(f'digits-photos/{text[1]}.png')
-        except FileNotFoundError:
-            try:
-                img = Image.open(f'digits-photos/{text[1]}.jpg')
-            except FileNotFoundError:
-                return error_handling(message)
+    elif wtf_reading(message.text, message):
+        return
 
-        bot.send_photo(message.chat.id, img)
+    elif the_world(message.text, message):
+        return
+
+    elif omg(message.text, message):
+        return
+
+    elif yare_yare(message.text, message):
+        return
+
     else:
         error_handling(message)
 
